773de4cb2ba017ab2b0c8fac9b2fa3915b036e4b
Ryan Flynn <ryanflynn44@gmail.com>
2024-06-06 21:19:44 -0400

    feat: added first release

---
+++ target/release/deps/rustor-78ed8ff8a3b5a0ce.d
@@ -0,0 +1,5 @@
+/Users/ryanflynn/Development/code/side-projects/rustor/target/release/deps/rustor-78ed8ff8a3b5a0ce: src/main.rs
+
+/Users/ryanflynn/Development/code/side-projects/rustor/target/release/deps/rustor-78ed8ff8a3b5a0ce.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/rustor.d
@@ -0,0 +1,1 @@
+/Users/ryanflynn/Development/code/side-projects/rustor/target/release/rustor: /Users/ryanflynn/Development/code/side-projects/rustor/src/main.rs
