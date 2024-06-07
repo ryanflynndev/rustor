df812e36cdfd327febc739c683b45b5163e4ae5f
Ryan Flynn <ryanflynn44@gmail.com>
2024-06-06 21:40:18 -0400

    fix: remove target

--- target/release/deps/rustor-78ed8ff8a3b5a0ce.d
+++
@@ -1,5 +0,0 @@
-/Users/ryanflynn/Development/code/side-projects/rustor/target/release/deps/rustor-78ed8ff8a3b5a0ce: src/main.rs
-
-/Users/ryanflynn/Development/code/side-projects/rustor/target/release/deps/rustor-78ed8ff8a3b5a0ce.d: src/main.rs
-
-src/main.rs:
--- target/release/rustor.d
+++
@@ -1,1 +0,0 @@
-/Users/ryanflynn/Development/code/side-projects/rustor/target/release/rustor: /Users/ryanflynn/Development/code/side-projects/rustor/src/main.rs
